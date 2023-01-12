027434ca32ee757a1e75131118b15425808b3659
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-12 18:33:07 +0200

    added trending TV

--- src/components/TrendingTV/TrendingTV.js
+++ src/components/TrendingTV/TrendingTV.js
@@ -1,5 +1,11 @@
 import css from './TrendingTV.module.scss';
 import { IoIosArrowForward } from 'react-icons/io';
+import Maturing from '../../assets/images/TrendingMovies/1.png';
+import Leap from '../../assets/images/TrendingMovies/2.png';
+import Oak from '../../assets/images/TrendingMovies/3.png';
+import Legal from '../../assets/images/TrendingMovies/4.png';
+import Interlock from '../../assets/images/TrendingMovies/5.png';
+import Echo from '../../assets/images/TrendingMovies/6.png';
 
 const TrendingTV = () => {
   return (
@@ -7,8 +13,29 @@ const TrendingTV = () => {
       <h2 className={css.title}>
         Trending TV Shows <IoIosArrowForward />
       </h2>
-      <div></div>
-      <button type="button"></button>
+      <ul className={css.list}>
+        <li>
+          <img src={Maturing} alt="Maturing" width="206" height="309" />
+        </li>
+        <li>
+          <img src={Leap} alt="Maturing" width="206" height="309" />
+        </li>
+        <li>
+          <img src={Oak} alt="Maturing" width="206" height="309" />
+        </li>
+        <li>
+          <img src={Legal} alt="Maturing" width="206" height="309" />
+        </li>
+        <li>
+          <img src={Interlock} alt="Maturing" width="206" height="309" />
+        </li>
+        <li>
+          <img src={Echo} alt="Maturing" width="206" height="309" />
+        </li>
+      </ul>
+      <button type="button" className={css.btn}>
+        See more
+      </button>
     </div>
   );
 };
