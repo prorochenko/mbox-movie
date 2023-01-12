79876cec90ebb4c12ef53be9d7000f0293446d15
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-12 16:09:34 +0200

    local files added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { HelmetProvider } from 'react-helmet-async';
 
 import { App } from './components/App';
 import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}>
