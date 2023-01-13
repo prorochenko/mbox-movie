cfe082cd8ecc410bd1fc46592b56372aecdfd704
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-13 14:56:46 +0200

    changed button to a link

--- src/components/PopularGenres/PopularGenres.js
+++ src/components/PopularGenres/PopularGenres.js
@@ -6,75 +6,75 @@ const PopularGenres = () => {
   return (
     <div className={css.box}>
       <h2 className={css.title}>
-        <a href="Popular">
+        <a href="popular">
           Popular Genres&nbsp;
           <IoIosArrowForward className={css.title__arrow} />
         </a>
       </h2>
       <ul className={css.list}>
         <li>
-          <button type="button" className={css.romance}>
+          <a href="romance" target="_blank" className={css.romance}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-hearts`}></use>
             </svg>
             <p className={css.text}>Romance</p>
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Drama}>
+          <a href="drama" target="_blank" className={css.Drama}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-cinema-1`}></use>
             </svg>
             Drama
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Historical}>
+          <a href="historical" target="_blank" className={css.Historical}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-documentary-1`}></use>
             </svg>
             Historical
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Action}>
+          <a href="action" target="_blank" className={css.Action}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-killer-1`}></use>
             </svg>
             Action
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Scifi}>
+          <a href="scifi" target="_blank" className={css.Scifi}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-Group-40`}></use>
             </svg>
             Sci-fi
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Horror}>
+          <a href="horror" target="_blank" className={css.Horror}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-ghost-1`}></use>
             </svg>
             Horror
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Comedy}>
+          <a href="comedy" target="_blank" className={css.Comedy}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-smile-1`}></use>
             </svg>
             Comedy
-          </button>
+          </a>
         </li>
         <li>
-          <button type="button" className={css.Documentary}>
+          <a href="documentary" target="_blank" className={css.Documentary}>
             <svg className={css.icon}>
               <use href={`${icons}#icon-castle-1`}></use>
             </svg>
             Documentary
-          </button>
+          </a>
         </li>
       </ul>
     </div>
