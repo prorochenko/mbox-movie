49915a7b24d909a3955e457ee6e6a60606b0fd69
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-13 13:47:16 +0200

    edit herp

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -23,40 +23,41 @@ const Hero = () => {
   }, 300);
 
   return (
-    <div className={css.box}>
-      <div className={css.rightPart}>
-        <h1 className={css.title}>Zack Snyder's Justice League</h1>
-        <div className={css.data}>
-          <div className={css.dataGroup}>
-            <p className={css.dataNumbers}>2018</p>
-            <p className={css.dataAge}>18+</p>
-            <p className={css.dataNumbers}>4h 2min</p>
+    <>
+      <div className={css.box}>
+        <div className={css.rightPart}>
+          <h1 className={css.title}>Zack Snyder's Justice League</h1>
+          <div className={css.data}>
+            <div className={css.dataGroup}>
+              <p className={css.dataNumbers}>2018</p>
+              <p className={css.dataAge}>18+</p>
+              <p className={css.dataNumbers}>4h 2min</p>
+            </div>
+            <p className={css.dataNumbers}>
+              8.8
+              <IconContext.Provider value={{ color: '#FFB800' }}>
+                <AiFillStar />
+              </IconContext.Provider>
+            </p>
           </div>
-          <p className={css.dataNumbers}>
-            8.8
-            <IconContext.Provider value={{ color: '#FFB800' }}>
-              <AiFillStar />
-            </IconContext.Provider>
+          <p className={css.text}>
+            Determined to ensure Superman's ultimate sacrifice was not in vain,
+            Bruce Wayne aligns forces with Diana Prince with plans to recruit a
+            team of metahumans to protect the world from an approaching threat
+            of catastrophic proportions.
           </p>
+          <div className={css.btns}>
+            <button type="button" className={css.buttonBlue}>
+              Watch
+            </button>
+            <button type="button" className={css.buttonWhite}>
+              More Information
+            </button>
+          </div>
         </div>
-        <p className={css.text}>
-          Determined to ensure Superman's ultimate sacrifice was not in vain,
-          Bruce Wayne aligns forces with Diana Prince with plans to recruit a
-          team of metahumans to protect the world from an approaching threat of
-          catastrophic proportions.
-        </p>
-        <div className={css.btns}>
-          <button type="button" className={css.buttonBlue}>
-            Watch
-          </button>
-          <button type="button" className={css.buttonWhite}>
-            More Information
-          </button>
-        </div>
-      </div>
-      {/* <div className={css.rightBar}> */}
-      <div id="mainPic" className={css.mainPic}></div>
-      {/* <picture className={css.picture}>
+        {/* <div className={css.rightBar}> */}
+        <div id="mainPic" className={css.mainPic}></div>
+        {/* <picture className={css.picture}>
           <source
             srcsset={`${Batman1x} 1x,
                   ${Batman2x} 2x`}
@@ -65,8 +66,9 @@ const Hero = () => {
           />
           <img src={Batman2x} alt="logo" loading="lazy" />
         </picture> */}
-    </div>
-    // </div>
+      </div>
+      <div className={css.bottom}></div>
+    </>
   );
 };
 
